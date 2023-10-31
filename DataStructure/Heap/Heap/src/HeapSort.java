cb642a486ea604dbb94a8a181f7af7ce2f8c654d
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-10-31 15:29:44 +0700

    Update HeapSort.java

--- DataStructure/Heap/Heap/src/HeapSort.java
+++ DataStructure/Heap/Heap/src/HeapSort.java
@@ -26,6 +26,6 @@ public class HeapSort {
     public static void exchange(int[] a, int i, int j) {
         a[i] += a[j];
         a[j] = a[i] - a[j];
-        a[j] = a[i] - a[j];
+        a[i] = a[i] - a[j];
     }
 }
