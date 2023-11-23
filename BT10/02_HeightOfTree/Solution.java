fac4d12b131fcfac012c64038d8fdb7b965e25b2
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-11-24 01:10:39 +0700

    BT10

---
+++ BT10/02_HeightOfTree/Solution.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+      	//the height of tree that has 0 or 1 node is 0
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 0;
+        
+        return Math.max(height(root.left) + 1, height(root.right) + 1);
+        
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
