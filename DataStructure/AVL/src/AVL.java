a1f129d7343871eb64c49f0b9d82bd164fd6e2de
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-12-11 10:04:56 +0700

    update AVL

--- DataStructure/AVL/src/AVL.java
+++ DataStructure/AVL/src/AVL.java
@@ -1,186 +1,207 @@
-// AVL tree implementation in Java
+import java.util.NoSuchElementException;
 
-// Create node
-class Node {
-    int item, height;
-    Node left, right;
+class AVL {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        int height;
 
-    Node(int d) {
-        item = d;
-        height = 1;
-    }
-}
-
-// Tree class
-class AVLTree {
-    Node root;
-
-    int height(Node N) {
-        if (N == null)
-            return 0;
-        return N.height;
-    }
-
-    int max(int a, int b) {
-        return (a > b) ? a : b;
-    }
-
-    Node rightRotate(Node y) {
-        Node x = y.left;
-        Node T2 = x.right;
-        x.right = y;
-        y.left = T2;
-        y.height = max(height(y.left), height(y.right)) + 1;
-        x.height = max(height(x.left), height(x.right)) + 1;
-        return x;
-    }
-
-    Node leftRotate(Node x) {
-        Node y = x.right;
-        Node T2 = y.left;
-        y.left = x;
-        x.right = T2;
-        x.height = max(height(x.left), height(x.right)) + 1;
-        y.height = max(height(y.left), height(y.right)) + 1;
-        return y;
-    }
-
-    // Get balance factor of a node
-    int getBalanceFactor(Node N) {
-        if (N == null)
-            return 0;
-        return height(N.left) - height(N.right);
-    }
-
-    // Insert a node
-    Node insertNode(Node node, int item) {
-
-        // Find the position and insert the node
-        if (node == null)
-            return (new Node(item));
-        if (item < node.item)
-            node.left = insertNode(node.left, item);
-        else if (item > node.item)
-            node.right = insertNode(node.right, item);
-        else
-            return node;
-
-        // Update the balance factor of each node
-        // And, balance the tree
-        node.height = 1 + max(height(node.left), height(node.right));
-        
-        int balanceFactor = getBalanceFactor(node);
-        
-        if (balanceFactor > 1) {
-            if (getBalanceFactor(node.left) >= 0) { //item < node.left.item
-                return rightRotate(node);
-            } else  { //if (item > node.left.item)
-                node.left = leftRotate(node.left);
-                return rightRotate(node);
-            }
-        }
-        if (balanceFactor < -1) {
-            if (getBalanceFactor(node.right) <= 0) { //item > node.right.item
-                return leftRotate(node);
-            } else  { // if (item < node.right.item)
-                node.right = rightRotate(node.right);
-                return leftRotate(node);
-            }
-        }
-        return node;
-    }
-
-    Node nodeWithMimumValue(Node node) {
-        Node current = node;
-        while (current.left != null)
-            current = current.left;
-        return current;
-    }
-
-    // Delete a node
-    Node deleteNode(Node root, int item) {
-
-        // Find the node to be deleted and remove it
-        if (root == null)
-            return root;
-        if (item < root.item)
-            root.left = deleteNode(root.left, item);
-        else if (item > root.item)
-            root.right = deleteNode(root.right, item);
-        else {
-            if ((root.left == null) || (root.right == null)) {
-                Node temp = null;
-                if (temp == root.left)
-                    temp = root.right;
-                else
-                    temp = root.left;
-                if (temp == null) {
-                    temp = root;
-                    root = null;
-                } else
-                    root = temp;
-            } else {
-                Node temp = nodeWithMimumValue(root.right);
-                root.item = temp.item;
-                root.right = deleteNode(root.right, temp.item);
-            }
-        }
-        if (root == null)
-            return root;
-
-        // Update the balance factor of each node and balance the tree
-        root.height = max(height(root.left), height(root.right)) + 1;
-        int balanceFactor = getBalanceFactor(root);
-        if (balanceFactor > 1) {
-            if (getBalanceFactor(root.left) >= 0) {
-                return rightRotate(root);
-            } else {
-                root.left = leftRotate(root.left);
-                return rightRotate(root);
-            }
-        }
-        if (balanceFactor < -1) {
-            if (getBalanceFactor(root.right) <= 0) {
-                return leftRotate(root);
-            } else {
-                root.right = rightRotate(root.right);
-                return leftRotate(root);
-            }
-        }
-        return root;
-    }
-
-    // Print the tree
-    private void printTree(Node currPtr, String indent, boolean last) {
-        if (currPtr != null) {
-            System.out.print(indent);
-            if (last) {
-                System.out.print("R----");
-                indent += "   ";
-            } else {
-                System.out.print("L----");
-                indent += "|  ";
-            }
-            System.out.println(currPtr.item);
-            printTree(currPtr.left, indent, false);
-            printTree(currPtr.right, indent, true);
+        public Node(int data) {
+            this.data = data;
+            height = 0;
         }
     }
+
+        Node root;
+
+
+        /*
+         * insert
+         * ********************************************************************************
+         */
+        public void insert(int data) {
+            root = insert(root, data);
+        }
+
+        private Node insert(Node x, int data) {
+            if (x == null) return new Node(data);
+
+            if (data < x.data) x.left = insert(x.left, data);
+            else if (data > x.data) x.right = insert(x.right, data);
+            //else return x;
+
+            x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+            return balance(x);
+        }
+
+        /*
+         * delete
+         * ********************************************************************************
+         */
+
+        public void delete(int data) {
+            root = delete(root, data);
+        }
+
+        private Node delete(Node x, int data) {
+            if (data < x.data) x.left = delete(x.left, data);
+            else if (data > x.data) x.right = delete(x.right, data);
+            else {
+                if (x.left == null) return x.right;
+                if (x.right == null) return x.left;
+
+                Node t = x;
+                x = min(t.right);
+                x.right = deleteMin(t.right);
+                x.left = t.left;
+            }
+
+            x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+            return balance(x);
+        }
+
+        private Node min(Node x) {
+            while (x.left != null) x= x.left;
+            return x;
+        }
+
+        private Node deleteMin(Node x) {
+            if (x.left == null) return x.right;
+            x.left = deleteMin(x.left);
+
+            x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+            return balance(x);
+        }
+
+        /*
+         * get
+         * ********************************************************************************
+         */
+
+        public Node get(int data) {
+            Node x = root;
+            x = get(x, data);
+            return x;
+        }
+
+        private  Node get(Node x, int data) {
+            if (x == null) return null;
+            if (data < x.data) return get(x.left, data);
+            else if (data > x.data) return get(x.right, data);
+            else return x;
+        }
+
+
+
+        /*
+         * height
+         * ********************************************************************************
+         */
+
+        public int height(Node x) {
+            if (x == null) return -1;
+            return x.height;
+        }
+
+        /*
+         * depth
+         * ********************************************************************************
+         */
+
+        public int depth(int data) {
+            return depth(root, data, 0);
+        }
+
+        private int depth(Node x, int data, int depth) {
+            if (x == null) return -1;
+
+            if (x.data ==  data) return depth;
+
+            int d = depth(x.left, data, depth + 1);
+            if (d != -1) return d;
+
+            return depth(x.right, data, depth + 1);
+        }
+
+        /*
+         * balance factor
+         * ********************************************************************************
+         */
+
+        private int balanceFactor(Node x) {
+            return height(x.left) - height(x.right);
+        }
+
+        /*
+         * balance
+         * ********************************************************************************
+         */
+
+        private Node balance(Node x) {
+            if (balanceFactor(x) > 1) {
+                if (balanceFactor(x.left) < 0) x.left = rotateLeft(x.left);
+                x = rotateRight(x);
+            }
+
+            if(balanceFactor(x) < -1) {
+                if (balanceFactor(x.right) > 0) x.right = rotateRight(x.right);
+                x = rotateLeft(x);
+            }
+            return x;
+        }
+
+        /*
+         * rotateLeft
+         * ********************************************************************************
+         */
+
+        private Node rotateLeft(Node x) {
+            Node y = x.right;
+            x.right = y.left;
+            y.left = x;
+
+            x.height = Math.max(height(x.left), height(x.right)) + 1;
+            y.height = Math.max(height(y.left), height(y.right)) + 1;
+            return y;
+        }
+
+        /*
+         * rotateRight
+         * ********************************************************************************
+         */
+        private Node rotateRight(Node x) {
+            Node y = x.left;
+            x.left = y.right;
+            y.right = x;
+
+            x.height = Math.max(height(x.left), height(x.right)) + 1;
+            y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+            return y;
+        }
 
     // Driver code
     public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
-        tree.root = tree.insertNode(tree.root, 33);
-        tree.root = tree.insertNode(tree.root, 13);
-        tree.root = tree.insertNode(tree.root, 53);
-        tree.root = tree.insertNode(tree.root, 9);
-        tree.root = tree.insertNode(tree.root, 21);
-        tree.root = tree.insertNode(tree.root, 61);
-        tree.root = tree.insertNode(tree.root, 8);
-        tree.root = tree.insertNode(tree.root, 11);
-        tree.printTree(tree.root, "", true);
-        tree.root = tree.deleteNode(tree.root, 13);
-        System.out.println("After Deletion: ");
-        tree.printTree(tree.root, "", true);
+        AVL tree = new AVL();
+        tree.insert(33);
+        tree.insert(13);
+        tree.insert(53);
+        tree.insert(9);
+        tree.insert(21);
+        tree.insert(61);
+        tree.insert(8);
+        tree.insert(11);
+
+        System.out.println(tree.height(tree.get(13))); // 2
+        System.out.println(tree.depth(8)); // 3
+        System.out.println(tree.depth(61)); //2
+        tree.delete(21);
+        System.out.println(tree.height(tree.get(13))); // 1
+        System.out.println(tree.depth(8)); // 2
+        System.out.println(tree.depth(61)); // 2
     }
 }
